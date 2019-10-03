e16056ae7f422b9e4f810b7bf5b4b1d2ee29cc76
Jim Hester <james.f.hester@gmail.com>
2019-10-03 14:48:42 -0400

    Try to debug build

--- src/installer.ts
+++ src/installer.ts
@@ -1,38 +1,38 @@
-let tempDirectory = process.env['RUNNER_TEMP'] || '';
+let tempDirectory = process.env["RUNNER_TEMP"] || "";
 
-import * as core from '@actions/core';
-import * as exec from '@actions/exec';
-import * as tc from '@actions/tool-cache';
-import * as util from 'util';
-import * as path from 'path';
-import {promises as fs} from 'fs';
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as tc from "@actions/tool-cache";
+import * as util from "util";
+import * as path from "path";
+import { promises as fs } from "fs";
 
-const IS_WINDOWS = process.platform === 'win32';
+const IS_WINDOWS = process.platform === "win32";
 
 if (!tempDirectory) {
   let baseLocation;
   if (IS_WINDOWS) {
     // On windows use the USERPROFILE env variable
-    baseLocation = process.env['USERPROFILE'] || 'C:\\';
+    baseLocation = process.env["USERPROFILE"] || "C:\\";
   } else {
-    if (process.platform === 'darwin') {
-      baseLocation = '/Users';
+    if (process.platform === "darwin") {
+      baseLocation = "/Users";
     } else {
-      baseLocation = '/home';
+      baseLocation = "/home";
     }
   }
-  tempDirectory = path.join(baseLocation, 'actions', 'temp');
+  tempDirectory = path.join(baseLocation, "actions", "temp");
 }
 
 export async function getR(version: string) {
-  let toolPath = tc.find('R', version);
+  let toolPath = tc.find("R", version);
 
   if (toolPath) {
     core.debug(`Tool found in cache ${toolPath}`);
   } else {
     // download, extract, cache
     toolPath = await acquireR(version);
-    core.debug('R is cached under ' + toolPath);
+    core.debug("R is cached under " + toolPath);
   }
 
   setupRLibrary();
@@ -60,21 +60,21 @@ async function acquireR(version: string): Promise<string> {
   //
   let extPath: string = tempDirectory;
   if (!extPath) {
-    throw new Error('Temp directory not set');
+    throw new Error("Temp directory not set");
   }
 
   try {
-  await exec.exec('installer', ['-pkg', downloadPath, '-target', '/'])
+    await exec.exec("installer", ["-pkg", downloadPath, "-target", "/"]);
   } catch (error) {
-    core.debug(error)
+    core.debug(error);
 
     throw `Failed to install R: ${error}`;
   }
 
   //if (osPlat == 'win32') {
-    //extPath = await tc.extractZip(downloadPath);
+  //extPath = await tc.extractZip(downloadPath);
   //} else {
-    //extPath = await tc.extractTar(downloadPath);
+  //extPath = await tc.extractTar(downloadPath);
   //}
 
   //
@@ -83,21 +83,21 @@ async function acquireR(version: string): Promise<string> {
   //const toolRoot = path.join(extPath, 'r');
   //version = normalizeVersion(version);
   //return await tc.cacheDir(toolRoot, 'r', version);
-  return '/'
+  return "/";
 }
 
 async function setupRLibrary() {
-    await fs.writeFile('~/.Rprofile', 'options(repos = "https://cloud.r-project.org")')
+  await fs.writeFile(
+    "~/.Rprofile",
+    'options(repos = "https://cloud.r-project.org")'
+  );
 }
 
 function getFileName(version: string): string {
-  const filename: string = util.format(
-    'R-%s',
-    version
-  );
+  const filename: string = util.format("R-%s", version);
   return filename;
 }
 
 function getDownloadUrl(filename: string): string {
-  return util.format('https://cloud.r-project.org/bin/macosx/%s', filename);
+  return util.format("https://cloud.r-project.org/bin/macosx/%s", filename);
 }
--- src/setup-r.ts
+++ src/setup-r.ts
@@ -1,10 +1,10 @@
-import * as core from '@actions/core';
-import {getR} from './installer';
+import * as core from "@actions/core";
+import { getR } from "./installer";
 
 async function run() {
   try {
     core.debug(`started action`);
-    let version = core.getInput('r-version');
+    let version = core.getInput("r-version");
     core.debug(`got version ${version}`);
     await getR(version);
   } catch (error) {
