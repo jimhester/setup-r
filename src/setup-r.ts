66765e51256d4c4def94a85c9fd948276e5de07f
Jim Hester <james.f.hester@gmail.com>
2019-10-03 15:12:50 -0400

    Try again with debug enabled

--- src/setup-r.ts
+++ src/setup-r.ts
@@ -4,9 +4,9 @@ import { getR } from "./installer";
 async function run() {
   try {
     core.debug(`started action`);
-    //let version = core.getInput("r-version");
-    //core.debug(`got version ${version}`);
-    //await getR(version);
+    let version = core.getInput("r-version");
+    core.debug(`got version ${version}`);
+    await getR(version);
   } catch (error) {
     core.setFailed(error.message);
   }
